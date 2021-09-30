08ab66619599eadb1c1071200e0ed102dae43ffd
Cristian Gonzalez <albertsevilla1996@gmail.com>
2021-09-30 17:49:34 -0600

    feat: :sparkles: Add user entity

---
+++ src/modules/user/entity/user.entity.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Status } from 'src/shared/status.enum';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Role } from '../../role/entity/role.entity';
+import { UserDetail } from './user_details.entity';
+
+@Entity('users')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', unique: true, length: 25, nullable: false })
+  username: string;
+
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  email: string;
+
+  @Column({ type: 'text', nullable: false })
+  password: string;
+
+  @OneToOne((type) => UserDetail, {
+    cascade: true,
+    nullable: false,
+    eager: true,
+  })
+  @JoinColumn({ name: 'detail_id' })
+  details: UserDetail;
+
+  @ManyToMany((type) => Role, (role) => role.users, { eager: true })
+  @JoinTable({ name: 'user_by_roles' })
+  roles: Role[];
+
+  @Column({ type: 'varchar', default: Status.ACTIVE })
+  status: string;
+
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at', nullable: true })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at', nullable: true })
+  updatedAt: Date;
+}
