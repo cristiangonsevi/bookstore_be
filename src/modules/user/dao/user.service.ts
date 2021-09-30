5d66940d2007a3d5eaa0d5438818673cac677c84
Cristian Gonzalez <albertsevilla1996@gmail.com>
2021-09-30 17:47:08 -0600

    feat: :sparkles: Add user service

---
+++ src/modules/user/dao/user.service.ts
@@ -0,0 +1,58 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { plainToClass } from 'class-transformer';
+import { Role } from '../../../modules/role/entity/role.entity';
+import { getConnection } from 'typeorm';
+import { Status } from '../../../shared/status.enum';
+import { ReadUserDto, UpdateUserDto } from '../dto';
+import { UserRepository } from '../entity/user.repository';
+import { UserDetail } from '../entity/user_details.entity';
+import { User } from '../entity/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private readonly _userRepository: UserRepository,
+  ) {}
+
+  async getUser(id: number): Promise<ReadUserDto> {
+    if (!id) throw new BadRequestException();
+    const user: ReadUserDto = await this._userRepository.findOne(id, {
+      where: { status: Status.ACTIVE },
+    });
+    if (!user) throw new NotFoundException();
+    // return user;
+    return plainToClass(ReadUserDto, user);
+  }
+
+  async getUsers(): Promise<ReadUserDto[]> {
+    const users: User[] = await this._userRepository.find({
+      where: { status: Status.ACTIVE },
+    });
+    if (!users) throw new NotFoundException();
+    return users.map((user) => plainToClass(ReadUserDto, user));
+  }
+
+  async createUser(user: User): Promise<ReadUserDto> {
+    const details = new UserDetail();
+    user.details = details;
+    const repo = await getConnection().getRepository(Role);
+    const defaultRole = await repo.findOne({ where: { name: 'GENERAL' } });
+    user.roles = [defaultRole];
+    await this._userRepository.save(user);
+    return plainToClass(ReadUserDto, user);
+  }
+
+  async updateUser(userId: number, data: UpdateUserDto): Promise<ReadUserDto> {
+    const user = await this._userRepository.findOne({ where: { id: userId } });
+    if (!user) throw new NotFoundException();
+    user.username = data.username;
+    await this._userRepository.save(user);
+    return plainToClass(ReadUserDto, user);
+  }
+}
