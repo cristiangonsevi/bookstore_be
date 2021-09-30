bd10c5bfcd4e7939ee72fc9eb81ab72bac046785
Cristian Gonzalez <albertsevilla1996@gmail.com>
2021-09-30 17:45:04 -0600

    feat: :sparkles: Add role entity

---
+++ src/modules/role/entity/role.entity.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Status } from 'src/shared/status.enum';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../user/entity/user.entity';
+
+@Entity('roles')
+export class Role extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 25, nullable: false })
+  name: string;
+
+  @Column({ type: 'text', nullable: false })
+  description: string;
+
+  @ManyToMany((type) => User, (user) => user.roles)
+  @JoinTable({ name: 'roles_by_users' })
+  users: User[];
+
+  @Column({ type: 'varchar', default: Status.ACTIVE })
+  status: string;
+
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at', nullable: true })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at', nullable: true })
+  updatedAt: Date;
+}
